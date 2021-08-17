f97f7ca654d3e5d7f7698e94901bf6142e615eca
Guillaume Ludinard <guillaume.ludi@gmail.com>
2021-08-17 19:53:37 +0900

    Lint and improvements

--- input/relp/setup.py
+++ input/relp/setup.py
@@ -1,18 +1,20 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name='snooze-relp',
-    version='1.0.0',
+    version='1.0.1',
     author='Guillaume Ludinard, Florian Dematraz',
     author_email='guillaume.ludi@gmail.com, ',
     description="Syslog input plugin for snooze server",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=url,
     packages=find_packages(include=['snooze_relp', 'snooze_relp.*']),
     entry_points={
         'console_scripts': [
-            'snooze-syslog = snooze_relp.main:main',
+            'snooze-relp = snooze_relp.main:main',
         ],
     },
     install_requires = [
