63dc7dd7f9427c000e8ad44ed6b0cdb1d9297a7a
Florian Dematraz <florian.dematraz@japannext.co.jp>
2022-07-22 17:43:12 +0900

    Web

---
+++ web/src/layouts/home/AppBar.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <v-app-bar
+      id="home-app-bar"
+      app
+      elevation="1"
+      elevate-on-scroll
+      height="80"
+    >
+      <base-img
+        :src="require('@/assets/logo.png')"
+        class="mr-3 hidden-xs-only"
+        contain
+        max-width="220"
+        width="100%"
+      />
+
+      <v-spacer />
+
+      <div>
+        <v-tabs
+          class="hidden-sm-and-down"
+          optional
+          background-color="transparent"
+        >
+          <v-tab
+            v-for="(name, i) in items"
+            :key="i"
+            :to="{ name }"
+            :exact="name === 'Home'"
+            :ripple="false"
+            class="font-weight-bold"
+            min-width="96"
+            text
+          >
+            {{ name }}
+          </v-tab>
+        </v-tabs>
+      </div>
+
+      <v-btn
+        class="font-weight-bold ml-6"
+        color="#11c684"
+        dark
+        depressed
+        href="https://snoozeweb.github.io/snooze/_build/html/getting_started/01_Installation.html"
+        rel="noopener"
+        target="_blank"
+        x-large
+      >
+        Get Started
+      </v-btn>
+
+      <v-app-bar-nav-icon
+        class="hidden-md-and-up"
+        @click="drawer = !drawer"
+      />
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeAppBar',
+
+    data: () => ({
+      drawer: null,
+      items: [
+        'Home',
+        'Pro',
+      ],
+    }),
+  }
+</script>
+
+<style lang="sass">
+  #home-app-bar
+    .v-tabs-slider
+      max-width: 24px
+      margin: 0 auto
+
+    .v-tab
+      &::before
+        display: none
+</style>
